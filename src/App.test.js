d9e11d5d5ab125cba42f0ea078264f59caff78c9
Alice Chang <sinsofangels@gmail.com>
2020-10-29 18:15:29 -0400

    Add testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,31 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
 
 test("renders App without crashing", () => {
   render(<App />);
+  const firstName = screen.getByLabelText(/first Name/i);
+  const lastName = screen.getByLabelText(/last Name/i);
+  const email = screen.getByLabelText(/email/i);
+  const message = screen.getByLabelText(/message/i);
+  const submit = screen.getByRole(/button/i)
+
+  fireEvent.change(firstName, {target: {value: "Alice"}});
+  fireEvent.change(lastName, {target: {value: "Chang"}});
+  fireEvent.change(email, {target: {value: "email@email.com"}});
+  fireEvent.change(message, {target: {value: "I ahve a message for you.w"}});
+
+  async ()=> {
+    fireEvent.click(submit);
+    await clearedFields (() => {
+      expect(firstName).toBeEmpty();
+      expect(lastName).toBeEmpty();
+      expect(email).toBeEmpty();
+      expect(message).toBeEmpty();
+      screen.getByText("Alice");
+      screen.getByText("Chang");
+      screen.getByText("email@email.com");
+      screen.getByText("I ahve a message for you.w");
+    });
+  };
 });
